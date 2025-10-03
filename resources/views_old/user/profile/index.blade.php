1745aa1a6bfcc0cd29c7a272438c3ba2ae4ec259
sayyidmct <sayyid@mct.co.id>
2025-10-03 10:15:36 +0700

    Update layout
    update design layout

--- resources/views_old/user/profile/index.blade.php
+++ resources/views_old/user/profile/index.blade.php
@@ -386,8 +386,7 @@
         &copy;
         <script>
             document.write(new Date().getFullYear())
-        </script> Metrica <span class="text-muted d-none d-sm-inline-block float-end">Crafted with <i
-                class="mdi mdi-heart text-danger"></i> by Mannatthemes</span>
+        </script> Transformasi Data Digital
     </footer>
     <!-- end Footer -->
     <!--end footer-->
